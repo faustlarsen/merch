3ebc47324079d3287d28596afdf4f650aec95198
Constantine <faustlarsen@gmail.com>
2021-02-11 22:13:23 -0700

    update productControl
    Co-authored-by: Nataliya Zhuravleva<natalindria@gmail.com>

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,18 +3,18 @@ import Product from "./Product";
 import PropTypes from "prop-types";
 
 function ProductList(props) {
-    return (
-        <React.Fragment>
-          {props.productList.map((product) =>
-            <Product 
-              whenProductClicked = {props.onProductSelection} 
-              name={product.name}
-              location={product.location}
-              quantity={product.quantity}
-              id={product.id} />
-          )}
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      {props.productList.map((product) =>
+        <Product 
+          whenProductClicked = {props.onProductSelection} 
+          name={product.name}
+          location={product.location}
+          quantity={product.quantity}
+          id={product.id} />
+      )}
+    </React.Fragment>
+  )
 }
 
 ProductList.propTypes = {
