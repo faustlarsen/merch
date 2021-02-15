71db58d5ee51a209f88034030089f7f4a0c64aed
Constantine <faustlarsen@gmail.com>
2021-02-15 11:21:23 -0700

    will fail adding product test

--- src/__tests__/reducers/product-list-reducer.test.js
+++ src/__tests__/reducers/product-list-reducer.test.js
@@ -2,7 +2,7 @@ import productListReducer from '../../reducers/product-list-reducer';
 
 describe('productListReducer', () => {
   let action;
-  const prpductData = {
+  const productData = {
     name: "Beer",
     location: "Home",
     quantity: 2,
@@ -13,5 +13,23 @@ describe('productListReducer', () => {
     expect(productListReducer({}, {type: null})).toEqual({});
 });
 
-
+   test('Should successfully add new product to masterProductList', () => {
+   const {name, location, quantity, id} = productData;
+   action = {
+     type: 'ADD_PRODUCT',
+     name: name,
+     location: location,
+     quantity: quantity,
+     id: id
+   }
+   expect(productListReducer({}, action)).toEqual({
+     [id] : {
+       name: name,
+       location: location,
+       quantity: quantity,
+       id: id
+     }
+   })
 })
+
+});
