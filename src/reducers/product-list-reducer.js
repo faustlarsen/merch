d60924c4c582cf5c960002e4a454af22e3020e58
Constantine <faustlarsen@gmail.com>
2021-02-15 11:25:16 -0700

    will pass adding product test

--- src/reducers/product-list-reducer.js
+++ src/reducers/product-list-reducer.js
@@ -1,4 +1,16 @@
 export default (state ={}, action) => {
-  return state;
-}
-
+  const {name, location, quantity, id} = action;
+  switch (action.type) {
+    case 'ADD_PRODUCT':
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        location: location,
+        quantity: quantity,
+        id: id
+      }
+    })
+  default:
+    return state;
+  }
+};
